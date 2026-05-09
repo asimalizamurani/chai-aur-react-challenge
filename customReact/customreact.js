514fdeb4b743d70aa2d4ed23b68dec7f8aa4dea0
Asim Ali <asimalizamurani@gmail.com>
2026-05-10 03:46:56 +0500

    Second methdo is easier because I'm uing a loop now multiple props or attributes can be handled.

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,8 +1,21 @@
 function customeRender(reactElement, container) {
+
+    // < ........ FIRST WAY .......... >
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.childern
+    // domElement.setAttribute('href', reactElement.props.href)
+    // domElement.setAttribute('target', reactElement.props.target)
+
+    // container.appendChild(domElement)
+
+    // < ........ SECOND WAY .......... >
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.childern
-    domElement.setAttribute('href', reactElement.props.href)
-    domElement.setAttribute('target', reactElement.props.target)
+
+    for (const prop in reactElement.props) {
+        if (prop === "children") continue // in early days of react, children was also passed as a prop, but now it is not passed as a prop, so we can ignore this line
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
 
     container.appendChild(domElement)
 
